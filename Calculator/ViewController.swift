6236576f32aab257dd6f728fac7370cb62b45ed7
Mehmet Ayaz <120301068@student.beykent.edu.tr>
2022-03-22 00:44:00 +0300

    finish code

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumberText: UITextField!
+    @IBOutlet weak var secondNumberText: UITextField!
+    @IBOutlet weak var resultNumberText: UILabel!
+    
+    var resultNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sum(_ sender: Any) {
+        if let firstNumber = Int(firstNumberText.text!){
+            if let secondNumber = Int(secondNumberText.text!){
+                resultNumber = firstNumber + secondNumber
+                resultNumberText.text = String(resultNumber)
+            }
+        }
+    }
+    @IBAction func subtraction(_ sender: Any) {
+        if let firstNumber = Int(firstNumberText.text!){
+            if let secondNumber = Int(secondNumberText.text!){
+                resultNumber = firstNumber - secondNumber
+                resultNumberText.text = String(resultNumber)
+            }
+        }
+    }
+    
+    @IBAction func multiplication(_ sender: Any) {
+        if let firstNumber = Int(firstNumberText.text!){
+            if let secondNumber = Int(secondNumberText.text!){
+                resultNumber = firstNumber * secondNumber
+                resultNumberText.text = String(resultNumber)
+            }
+        }
+    }
+    @IBAction func division(_ sender: Any) {
+        if let firstNumber = Int(firstNumberText.text!){
+            if let secondNumber = Int(secondNumberText.text!){
+                resultNumber = firstNumber / secondNumber
+                resultNumberText.text = String(resultNumber)
+            }
+        }
+    }
 }
 
